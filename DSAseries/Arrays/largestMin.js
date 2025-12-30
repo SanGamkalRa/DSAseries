433f521c3dcbed928e1fca45dc77358a7274b285
SanGamkalRa <38105591+SanGamkalRa@users.noreply.github.com>
2025-12-30 14:27:43 +1300

    Create largestMin.js

---
+++ DSAseries/Arrays/largestMin.js
@@ -0,0 +1,20 @@
+let values = [5,15,22,1,-15,24]
+
+const Fun=(values)=>{
+    
+    let min = Number.POSITIVE_INFINITY
+    let indx = 0
+    for(let i = 0;i<=values.length;i++){
+        if(values[i]<min){
+            min=values[i]
+            indx = i
+        }
+        
+    }
+    console.log(min,indx)
+    
+    
+}
+
+
+Fun(values)
